56033f710242dc80a04a1ed07f22b0164c2f3541
Anton Gorchakov <pm13gai@gmail.com>
2021-03-31 00:02:32 +0300

    Updates

---
+++ precache-manifest.c492852dc216e52bb7d4600ee0881b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fcf585ab7ecc3b4b131c977841bce60",
+    "url": "/test_ex/index.html"
+  },
+  {
+    "revision": "ca29535a5a916660e4ad",
+    "url": "/test_ex/static/css/main.6b935b5d.chunk.css"
+  },
+  {
+    "revision": "fe42cb861e01af72175b",
+    "url": "/test_ex/static/js/2.aaea350c.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/test_ex/static/js/2.aaea350c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca29535a5a916660e4ad",
+    "url": "/test_ex/static/js/main.d994aaee.chunk.js"
+  },
+  {
+    "revision": "f7195b600d934142f447",
+    "url": "/test_ex/static/js/runtime-main.1bc8e555.js"
+  }
+]);
